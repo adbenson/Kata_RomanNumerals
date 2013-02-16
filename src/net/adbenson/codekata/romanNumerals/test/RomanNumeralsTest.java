40b3814c19e8736ae23af61be3009382e36eb262
Andrew D Benson <drew@adbenson.net>
2013-02-16 12:17:13 -0500

    Numbering tests for further reference

--- src/net/adbenson/codekata/romanNumerals/test/RomanNumeralsTest.java
+++ src/net/adbenson/codekata/romanNumerals/test/RomanNumeralsTest.java
@@ -7,37 +7,37 @@ import org.junit.Test;
 
 public class RomanNumeralsTest {
 	
-	@Test
+	@Test //1
 	public void romanNumerals1returnsI() {
 		assertEquals("I", RomanNumerals.roman(1));
 	}
 	
-	@Test
+	@Test //2
 	public void romanNumerals2returnsII() {
 		assertEquals("II", RomanNumerals.roman(2));
 	}
 	
-	@Test
+	@Test //3
 	public void romanNumerals3returnsIII() {
 		assertEquals("III", RomanNumerals.roman(3));
 	}
 	
-	@Test
+	@Test //4
 	public void romanNumerals4returnsIV() {
 		assertEquals("IV", RomanNumerals.roman(4));
 	}
 
-	@Test
+	@Test //5
 	public void romanNumerals5returnsV() {
 		assertEquals("V", RomanNumerals.roman(5));
 	}
 	
-	@Test
+	@Test //6
 	public void romanNumerals6returnsVI() {
 		assertEquals("VI", RomanNumerals.roman(6));
 	}
 	
-	@Test
+	@Test //7
 	public void romanNumerals8returnsVIII() {
 		assertEquals("VIII", RomanNumerals.roman(8));
 	}
