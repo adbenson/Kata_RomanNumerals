a64a06590c2e5beae449087eeb37a59f81bcb874
Andrew D Benson <drew@adbenson.net>
2013-02-16 11:55:26 -0500

    Passes test 2: 2 returns II

--- src/net/adbenson/codekata/romanNumerals/RomanNumerals.java
+++ src/net/adbenson/codekata/romanNumerals/RomanNumerals.java
@@ -3,7 +3,12 @@ package net.adbenson.codekata.romanNumerals;
 public class RomanNumerals {
 
 	public static Object roman(int i) {
-		return "I";
+		if (i == 1) {
+			return "I";
+		}
+		else {
+			return "II";
+		}
 	}
 
 }
