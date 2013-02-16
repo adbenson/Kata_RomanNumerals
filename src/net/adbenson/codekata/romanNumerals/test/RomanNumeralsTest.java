a9b3af33ea2e3177a15981b7b51d55946251d14e
Andrew D Benson <drew@adbenson.net>
2013-02-16 11:24:03 -0500

    First test: 1 returns I

--- src/net/adbenson/codekata/romanNumerals/test/RomanNumeralsTest.java
+++ src/net/adbenson/codekata/romanNumerals/test/RomanNumeralsTest.java
@@ -1,5 +1,14 @@
 package net.adbenson.codekata.romanNumerals.test;
 
+import net.adbenson.codekata.romanNumerals.RomanNumerals;
+
+import org.junit.Test;
+
 public class RomanNumeralsTest {
+	
+	@Test
+	public void romanNumerals1returnsI() {
+		assertEquals("I", RomanNumerals.roman(1));
+	}
 
 }
