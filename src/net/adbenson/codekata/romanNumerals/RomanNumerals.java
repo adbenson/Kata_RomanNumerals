aa92a9dfb3e6e18aad83817254ea7c8c96ffd3ed
Andrew D Benson <drew@adbenson.net>
2013-02-16 12:18:07 -0500

    Test 8 passes

--- src/net/adbenson/codekata/romanNumerals/RomanNumerals.java
+++ src/net/adbenson/codekata/romanNumerals/RomanNumerals.java
@@ -9,6 +9,7 @@ public class RomanNumerals {
 	
 	static {
 		numerals = new LinkedHashMap<Integer, String>();
+		numerals.put(9, "IX");
 		numerals.put(5, "V");
 		numerals.put(4, "IV");
 		numerals.put(1, "I");
